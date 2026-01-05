ef9b4da04da7c130af29b0ea452ea5764bcd128e
hoangtung386 <levuhoangtung1542003@gmail.com>
2026-01-05 19:03:23 +0700

    Enable CPU Offload

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ try:
     quantization_config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_compute_dtype=torch.float16,
-        bnb_4bit_quant_type="nf4"
+        bnb_4bit_quant_type="nf4",
+        llm_int8_enable_fp32_cpu_offload=True
     )
     
     tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
